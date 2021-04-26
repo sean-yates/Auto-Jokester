3b96f9ef15090847946f92c3b1d8b73749d7de06
Randy Elkins <randy.e.elkins@gmail.com>
2021-04-25 22:17:15 -0700

    setup other apis (jokeById, jokeByType, jokeSearch), as well as created a base.html to link to all the pages with those APIs

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,4 +4,9 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('randomJoke/', views.randomJoke, name='randomJoke'),
+    path('jokeById/', views.jokeById, name='jokeById'),
+    path('jokeByType/', views.jokeByType, name='jokeByType'),
+    path('jokeSearch/', views.jokeSearch, name='jokeSearch'),
+
+    # path('accounts/signup/', views.signup, name='signup'),
 ]
