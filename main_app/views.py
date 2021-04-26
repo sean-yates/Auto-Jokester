9a83098b71e240ce7356a89af8c8a1d088607ef9
Randy Elkins <randy.e.elkins@gmail.com>
2021-04-25 20:32:20 -0700

    initial API setup for random jokes

--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,29 @@
 from django.shortcuts import render
+# import requests
+API_KEY = '4967ac58d9msh8e3af7a90bbc99dp19e443jsnc0ddfc3ec16a'
 
 # Create your views here.
 
-
 def home(request):
     return render(request, 'home.html')
+
+def randomJoke(request):
+    import requests
+
+    url = "https://dad-jokes.p.rapidapi.com/random/joke"
+
+    headers = {
+        'x-rapidapi-key': "4967ac58d9msh8e3af7a90bbc99dp19e443jsnc0ddfc3ec16a",
+        'x-rapidapi-host': "dad-jokes.p.rapidapi.com"
+        }
+
+    response = requests.request("GET", url, headers=headers)
+
+    context = { 'response': response }
+
+    print(response.text)
+    return render(request, 'randomJoke.html', context)
+
+
+
+
