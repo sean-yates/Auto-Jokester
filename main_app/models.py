fed241515ee85f1367fe44a05c7ad997e64cb642
Sean Yates <seanyates@Seans-MacBook-Pro.local>
2021-04-27 18:45:15 -0700

    adds ability to sign up an account and log in

--- main_app/models.py
+++ main_app/models.py
@@ -4,13 +4,3 @@ from datetime import date
 from django.contrib.auth.models import User
 
 # Create your models here.
-class User(models.Model):
-    name = models.CharField(max_length=100)
-    email = models.CharField(max_length=100)
-    
-    def __str__(self):
-        return self.name
-    
-    # Add this method
-    def get_absolute_url(self):
-        return reverse('detail', kwargs={'user_id': self.id})
