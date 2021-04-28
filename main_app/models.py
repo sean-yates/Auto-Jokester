6b991a9c4bc06bc563076896f9d64855654467d6
Sean Yates <seanyates@Seans-MacBook-Pro.local>
2021-04-27 21:14:24 -0700

    adds joke model

--- main_app/models.py
+++ main_app/models.py
@@ -4,3 +4,23 @@ from datetime import date
 from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Joke(models.Model):
+    joke = models.CharField(max_length=10000) # what is the max length from the apis?
+    
+    source = models.CharField(max_length=50, blank=True, null=True,)
+
+    favorites = models.ManyToManyField(User, blank=True, related_name = 'favorites')
+    
+    dislikes = models.ManyToManyField(User, blank=True, related_name = 'dislikes')
+
+    category = models.CharField(max_length=50, blank=True, null=True)
+
+    createdBy = models.ForeignKey(User, blank=True, null=True, on_delete=models.SET_NULL)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return f'{self.joke}'
