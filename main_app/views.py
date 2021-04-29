2a98e6832db1ecbb999273920b1c0a6f3c8a1ff1
Randy Elkins <randy.e.elkins@gmail.com>
2021-04-28 21:15:14 -0700

    add random and searched jokes to db

--- main_app/views.py
+++ main_app/views.py
@@ -5,9 +5,10 @@ from django.contrib.auth.forms import UserCreationForm
 
 # import requests
 import datetime
+from .models import Joke
 from .forms import JokeForm
 
-API_KEY = '4967ac58d9msh8e3af7a90bbc99dp19e443jsnc0ddfc3ec16a'
+# API_KEY = '4967ac58d9msh8e3af7a90bbc99dp19e443jsnc0ddfc3ec16a'
 
 # Create your views here.
 def home(request):
@@ -29,12 +30,12 @@ def myfavoritejokes(request):
     return render(request, 'user/myfavoritejokes.html')
 
 def randomJoke(request):
+    # From icanhazdadjoke.com
     import requests
     headers = {
         'Accept': 'application/json',
         'User-Agent': 'My Library (https://github.com/sean-yates/Auto-Jokester)'
     }
-    # data = {'User-Agent': 'My Library (https://github.com/sean-yates/Auto-Jokester)'}
     response = requests.request("GET", 'https://icanhazdadjoke.com', headers=headers)
     context = { 'response': response }
 
@@ -42,94 +43,66 @@ def randomJoke(request):
 
     print('\033[30;206;48;2;255;255;0m', response_data['joke'], '\033[0m')
 
-    newJoke = {
-        'joke':response_data['joke'], 
-        'source':'icanhazdadjoke.com',
-        'category':'Dad Joke',
-        'createdBy':None,
-        }
-
-    form = JokeForm(newJoke)
-    if form.is_valid():
-        new_joke = form.save(commit=False)
-        new_joke.save()
+    save_joke_to_db(response_data['joke'])
 
     return render(request, 'randomJoke.html', context)
 
-
-
-def randomJoke_old(request):
-    # Ref. https://rapidapi.com/KegenGuyll/api/dad-jokes/endpoints
-    # On left-hand side, click "Random" ==> select "GET Random Jokes"
-    # On right-hand side, click "Code Snippets" --> select "Python" ==> "Requests" from dropdown menu
+def searchJoke(request):
+    # From icanhazdadjoke.com
     import requests
-    url = "https://dad-jokes.p.rapidapi.com/random/joke"
     headers = {
-        'x-rapidapi-key': "4967ac58d9msh8e3af7a90bbc99dp19e443jsnc0ddfc3ec16a",
-        'x-rapidapi-host': "dad-jokes.p.rapidapi.com"
-        }
-    response = requests.request("GET", url, headers = headers)
+        'Accept': 'application/json',
+        'User-Agent': 'My Library (https://github.com/sean-yates/Auto-Jokester)'
+    }
+    response = requests.request("GET", 'https://icanhazdadjoke.com/search?term=computer', headers=headers)
     context = { 'response': response }
-    print(response.text)
+
+    response_data = response.json()
+
+    print('\033[30;206;48;2;255;255;0m', response_data['results'][0], '\033[0m')
+    
+    save_joke_to_db(response_data['results'][0]['joke'])
+
     return render(request, 'randomJoke.html', context)
+
+def save_joke_to_db(incoming_joke):
+    existing_joke = Joke.objects.filter(joke__contains=incoming_joke)
+    print('\033[30;206;48;2;255;0;0m', len(existing_joke))
+    if len(existing_joke) == 0:
+        newJoke = {
+            'joke':incoming_joke, 
+            'source':'icanhazdadjoke.com',
+            'category':'D',
+            'createdBy':None,
+            }
+
+        form = JokeForm(newJoke)
+        if form.is_valid():
+            new_joke = form.save(commit=False)
+            new_joke.save()
+    else:
+        print('Joke already exists.')
 
 def jokeById(request):
-    # Ref. https://rapidapi.com/KegenGuyll/api/dad-jokes/endpoints ==> select "GET JokeByID"
-    # On left-hand side, click "Random" ==> select "GET JokeByID"
-    # On right-hand side, click "Code Snippets" --> select "Python" ==> "Requests" from dropdown menu
-    import requests
-    url = "https://dad-jokes.p.rapidapi.com/joke/5f80ccd641785ba7c7d27b4b"
-    headers = {
-        'x-rapidapi-key': "4967ac58d9msh8e3af7a90bbc99dp19e443jsnc0ddfc3ec16a",
-        'x-rapidapi-host': "dad-jokes.p.rapidapi.com"
-        }
-    response = requests.request("GET", url, headers = headers)
-    context = { 'response': response }
-    print(response.text)
-    return render(request, 'jokeById.html', context)
+    pass
 
 def jokeByType(request):
-    # Ref. https://rapidapi.com/KegenGuyll/api/dad-jokes/endpoints ==> select "GET JokeByType"
-    # On left-hand side, click "Random" ==> select "GET JokeByType"
-    # On right-hand side, click "Code Snippets" --> select "Python" ==> "Requests" from dropdown menu
-    import requests
-    url = "https://dad-jokes.p.rapidapi.com/joke/type/general"
-    headers = {
-        'x-rapidapi-key': "4967ac58d9msh8e3af7a90bbc99dp19e443jsnc0ddfc3ec16a",
-        'x-rapidapi-host': "dad-jokes.p.rapidapi.com"
-        }
-    response = requests.request("GET", url, headers = headers)
-    context = { 'response': response }
-    print(response.text)
-    return render(request, 'jokeByType.html', context)
+    pass
 
 def jokeSearch(request):
-    # Ref. https://rapidapi.com/KegenGuyll/api/dad-jokes/endpoints ==> select "GET Search"
-    # On left-hand side, click "Random" ==> select "GET Search"
-    # On right-hand side, click "Code Snippets" --> select "Python" ==> "Requests" from dropdown menu
-    import requests
-    url = "https://dad-jokes.p.rapidapi.com/joke/search"
-    querystring = {"term":"cow"}
-    headers = {
-        'x-rapidapi-key': "4967ac58d9msh8e3af7a90bbc99dp19e443jsnc0ddfc3ec16a",
-        'x-rapidapi-host': "dad-jokes.p.rapidapi.com"
-        }
-    response = requests.request("GET", url, headers = headers, params = querystring)
-    context = { 'response': response }
-    print(response.text)
-    return render(request, 'jokeSearch.html', context)
+    pass
 
 
 def signup(request):
-  error_message = ''
-  if request.method == 'POST':
-    form = UserCreationForm(request.POST)
-    if form.is_valid():
-      user = form.save()
-      login(request, user)
-      return redirect('/')
-    else:
-      error_message = 'Invalid sign up - try again'
-  form = UserCreationForm()
-  context = {'form': form, 'error_message': error_message}
-  return render(request, 'registration/signup.html', context)
+    error_message = ''
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('/')
+        else:
+            error_message = 'Invalid sign up - try again'
+    form = UserCreationForm()
+    context = {'form': form, 'error_message': error_message}
+    return render(request, 'registration/signup.html', context)
