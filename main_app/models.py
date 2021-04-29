c164da45d83ee9131da32a010cd55903fca3718e
Sean Yates <seanyates@Seans-MacBook-Pro.local>
2021-04-28 20:13:51 -0700

    updates Joke Model to use choices for category field and include approved field

--- main_app/models.py
+++ main_app/models.py
@@ -5,6 +5,16 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+CATEGORIES = (
+    'Random Jokes',
+    'Yo Mama Jokes',
+    'Dad Puns',
+    'Knock Knock Jokes',
+    'Bar Jokes',
+    'Computer Jokes',
+    'Sports Jokes',
+    'Dog Jokes'
+)
 
 class Joke(models.Model):
     joke = models.CharField(max_length=10000) # what is the max length from the apis?
@@ -15,11 +25,13 @@ class Joke(models.Model):
     
     dislikes = models.ManyToManyField(User, blank=True, related_name = 'dislikes')
 
-    category = models.CharField(max_length=50, blank=True, null=True)
+    category = models.CharField(max_length=50, choices=CATEGORIES, default=CATEGORIES[0])
 
     createdBy = models.ForeignKey(User, blank=True, null=True, on_delete=models.SET_NULL)
 
     created_at = models.DateTimeField(auto_now_add=True)
+
+    appproved = models.BooleanField(default=False)
 
 
     def __str__(self):
