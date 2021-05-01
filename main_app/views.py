9c8665cde2747d665b203a2de5447b967d693c32
Randy Elkins <randy.e.elkins@gmail.com>
2021-04-30 21:02:41 -0700

    update allJokes page to iterate thru joke categories, & auto-create links to appropriate links

--- main_app/views.py
+++ main_app/views.py
@@ -5,7 +5,7 @@ from django.contrib.auth.forms import UserCreationForm
 
 # import requests
 import datetime
-from .models import Joke
+from .models import Joke, CATEGORIES
 from .forms import JokeForm
 
 # API_KEY = '4967ac58d9msh8e3af7a90bbc99dp19e443jsnc0ddfc3ec16a'
@@ -15,7 +15,11 @@ def home(request):
     return render(request, 'home.html')
 
 def allJokes(request):
-    return render(request, 'allJokes.html')
+    # jokes = Joke.objects.order_by('id')
+    jokes = CATEGORIES
+    context = {'jokes': jokes}
+    print('context =', context)
+    return render(request, 'allJokes.html', context)
 
 def submitjoke(request):
     return render(request, 'submitjoke.html')
@@ -29,7 +33,50 @@ def profilePage(request):
 def myfavoritejokes(request):
     return render(request, 'user/myfavoritejokes.html')
 
-def randomJoke(request):
+    # path('<str:category>_joke_random/', views.joke_random, name='joke_random'),
+    # path('<str:category>_joke_by_id/', views.joke_by_id, name='joke_by_id'),
+    # path('<str:category>_joke_search/', views.joke_search, name='joke_search'),
+
+def joke_random(request, category):
+    import requests
+    if category == 'dad':
+        # From icanhazdadjoke.com
+        headers = {
+            'Accept': 'application/json',
+            'User-Agent': 'My Library (https://github.com/sean-yates/Auto-Jokester)'
+        }
+        response = requests.request("GET", 'https://icanhazdadjoke.com', headers=headers)
+    elif category == 'yomama':
+        # From icanhazdadjoke.com
+        headers = {
+            'Accept': 'application/json',
+            'User-Agent': 'My Library (https://github.com/sean-yates/Auto-Jokester)'
+        }
+        response = requests.request("GET", 'https://api.yomomma.info', headers=headers)
+    elif category == 'knockknock':
+        print('category =', category)
+    elif category == 'bar':
+        print('category =', category)
+    elif category == 'computer':
+        print('category =', category)
+    elif category == 'sports':
+        print('category =', category)
+    elif category == 'animal':
+        print('category =', category)
+
+
+
+    context = { 'response': response }
+
+    response_data = response.json()
+
+    print('\033[30;206;48;2;255;255;0m', response_data['joke'], '\033[0m')
+
+    save_joke_to_db(response_data['joke'])
+
+    return render(request, 'randomJoke.html', context)
+
+def random_dad_joke(request):
     # From icanhazdadjoke.com
     import requests
     headers = {
@@ -83,13 +130,13 @@ def save_joke_to_db(incoming_joke):
     else:
         print('Joke already exists.')
 
-def jokeById(request):
+def joke_by_id(request):
     pass
 
 def jokeByType(request):
     pass
 
-def jokeSearch(request):
+def joke_search(request):
     pass
 
 
