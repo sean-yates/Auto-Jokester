bceebaa1929ea2cf0049c5205ccbaf6f8100a080
Kaye Roberts <pkaye@Kayes-MacBook-Pro.local>
2021-05-01 16:48:12 -0700

    Event Listener Added

--- main_app/static/js/secret.js
+++ main_app/static/js/secret.js
@@ -1,36 +1,6 @@
-// Thanks StackOverflow!
-
-const correctKeys = {
-    72: 'h'
-    65: 'a',
-  };
-  
-  const secretCode = ['h', 'a', 'h', 'a', 'h', 'a'];
-  
-  var codePosition = 0;
-  
-  document.addEventListener('keydown', function(e) {
-    let key = allowedKeys[e.keyCode];
-    let requiredKey = secretCode[codePosition];
-  
-    if (key == requiredKey) {
-
-      codePosition++;
-  
-      if (codePosition == secretCode.length) {
-        activateCheats();
-        codePosition = 0;
-      }
-    } else {
-      codePosition = 0;
-    }
-  });
-  
-  function activateCheats() {
-    // document.body.style.backgroundImage = "url('images/cheatBackground.png')";
-  
-    // var audio = new Audio('audio/pling.mp3');
-    // audio.play();
-  
-    alert("cheats activated");
+document.addEventListener("keydown", function (event) {
+  code = "1";
+  if (event.key === code) {
+    document.body.style = "background-color: pink";
   }
+});
