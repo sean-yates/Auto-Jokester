51d7518175428795c2a8dca72c7d761c90c301bf
Sean Yates <seanyates@Seans-MacBook-Pro.local>
2021-05-05 18:12:21 -0700

    adds moderator field to profile and changes profile to show username in admin panel

--- main_app/models.py
+++ main_app/models.py
@@ -65,6 +65,9 @@ class Comment(models.Model):
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.TextField(max_length=500, blank=True)
+    moderator = models.BooleanField(default=False)
+    def __str__(self):
+        return f'{self.user.username}'
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
